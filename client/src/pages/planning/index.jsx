55b641749382aac74ce68e38ebc936774d519094
Juanjo <jj.cergoma@gmail.com>
2025-02-13 15:29:43 +0100

    adding html table

--- client/src/pages/planning/index.jsx
+++ client/src/pages/planning/index.jsx
@@ -5,17 +5,74 @@ import RecipeItem from "../../components/recipe-list";
 export default function Planning() {
   const { planningList } = useContext(GlobalContext);
 
+  // Timetable for the week
+  const weeklyTimetable = {
+    Monday: {
+      lunch: "",
+      dinner: ""
+    },
+    Tuesday: {
+      lunch: "",
+      dinner: ""
+    },
+    Wednesday: {
+      lunch: "",
+      dinner: ""
+    },
+    Thursday: {
+      lunch: "",
+      dinner: ""
+    },
+    Friday: {
+      lunch: "",
+      dinner: ""
+    },
+    Saturday: {
+      lunch: "",
+      dinner: ""
+    },
+    Sunday: {
+      lunch: "",
+      dinner: ""
+    }
+  }
+
   return (
     <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
-      {planningList && planningList.length > 0 ? (
-        planningList.map((item) => <RecipeItem item={item} />)
-      ) : (
-        <div>
-          <p className="lg:text-4x1 text-xl text-center text-black font-extrabold">
-            Nothing is added in planning
-          </p>
-        </div>
-      )}
+      {/* Timetable Display */}
+      <div className="w-full">
+        <table className="min-w-full border-collapse">
+          <thead>
+            <tr>
+              <th className="border p-2">Day</th>
+              <th className="border p-2">Lunch</th>
+              <th className="border p-2">Dinner</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(weeklyTimetable).map(([day, meals]) => (
+              <tr key={day}>
+                <td className="border p-2">{day}</td>
+                <td className="border p-2">{meals.lunch}</td>
+                <td className="border p-2">{meals.dinner}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Recipe Planning List */}
+      <div className="w-full">
+        {planningList && planningList.length > 0 ? (
+          planningList.map((item, index) => <RecipeItem key={index} item={item} />)
+        ) : (
+          <div>
+            <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+              Nothing is added in planning
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
