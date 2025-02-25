1f02bcfc0431b8fe9a43fef9302c8692ad547ce6
Juanjo <jj.cergoma@gmail.com>
2025-02-25 18:51:18 +0100

    add recipe into saved page

--- client/src/pages/savedRecipes/index.jsx
+++ client/src/pages/savedRecipes/index.jsx
@@ -1,21 +1,42 @@
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import { GlobalContext } from "../../context";
 import RecipeItem from "../../components/recipe-list";
+import { useGetUserID } from "../../hooks/useGetUserID";
+import axios from "axios";
 
 export default function SavedRecipes() {
-  const { planningList } = useContext(GlobalContext);
 
-  return (
-    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
-      {planningList && planningList.length > 0 ? (
-        planningList.map((item) => <RecipeItem item={item} />)
-      ) : (
-        <div>
-          <p className="lg:text-4x1 text-xl text-center text-black font-extrabold">
-            Nothing is added in planning
-          </p>
-        </div>
-      )}
-    </div>
-  );
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchSavedRecipes();
+  }, []);
+
+  
+    return (
+      // Muestra todas la lista de card si hay contenido en recipeList
+      <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+        {savedRecipes && savedRecipes.length > 0 ? (
+          savedRecipes.map((item) => <RecipeItem item={item} />)
+        ) : (
+          <div>
+            <p className="lg:text-4x1 text-xl text-center text-black font-extrabold">
+              Nothing to show. Please search something
+            </p>
+          </div>
+        )}
+      </div>
+    );
 }
